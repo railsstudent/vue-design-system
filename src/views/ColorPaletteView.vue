9e7ec6262dad0f6200c2b381ebd3d8ff1c18649f
Connie Leung <cleung334@gmail.com>
2022-12-29 13:06:02 +0800

    chore: initial commit

---
+++ src/views/ColorPaletteView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="color-palette">
+    <h1>This is a color palatte page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .color-palatte {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
